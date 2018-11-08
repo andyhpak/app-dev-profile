40722171963aef3f44d3712eca1e9983c1b50949
Andy Pak <andyhpak@protonmail.ch>
2018-11-08 21:44:57 +0900

    initial xcode project

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Andy on 2018/11/08.
+//  Copyright © 2018 Andy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
